90bb7459f16e74392d6608a98329d330af5fa39e
Mahfuz  Ahmed <159001317+mahfux090@users.noreply.github.com>
2024-03-05 20:03:58 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function convert() {
+    const numberInput = document.getElementById('number').value;
+    const fromBase = document.getElementById('from').value;
+    const toBase = document.getElementById('to').value;
+
+    let result;
+
+    if (fromBase === 'binary') {
+        if (toBase === 'decimal') {
+            result = parseInt(numberInput, 2).toString(10);
+        } else if (toBase === 'hexadecimal') {
+            result = parseInt(numberInput, 2).toString(16);
+        } else {
+            result = numberInput;
+        }
+    } else if (fromBase === 'decimal') {
+        if (toBase === 'binary') {
+            result = parseInt(numberInput, 10).toString(2);
+        } else if (toBase === 'hexadecimal') {
+            result = parseInt(numberInput, 10).toString(16);
+        } else {
+            result = numberInput;
+        }
+    } else if (fromBase === 'hexadecimal') {
+        if (toBase === 'binary') {
+            result = parseInt(numberInput, 16).toString(2);
+        } else if (toBase === 'decimal') {
+            result = parseInt(numberInput, 16).toString(10);
+        } else {
+            result = numberInput;
+        }
+    }
+
+    document.getElementById('result').innerText = `Result: ${result}`;
+}
+function reset() {
+    document.getElementById('number').value = '';
+    document.getElementById('from').selectedIndex = 0;
+    document.getElementById('to').selectedIndex = 0;
+    document.getElementById('result').innerText = '';
+}
